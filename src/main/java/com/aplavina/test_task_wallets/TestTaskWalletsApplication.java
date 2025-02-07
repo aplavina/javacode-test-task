5346ae49f48fbd851544f32f7d69c53ccc763025
AnnaAplavina <59871896+AnnaAplavina@users.noreply.github.com>
2025-02-07 11:22:24 +0300

    Initial commit

---
+++ src/main/java/com/aplavina/test_task_wallets/TestTaskWalletsApplication.java
@@ -0,0 +1,13 @@
+package com.aplavina.test_task_wallets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskWalletsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskWalletsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aplavina/test_task_wallets/TestTaskWalletsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplavina.test_task_wallets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskWalletsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
